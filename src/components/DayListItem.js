7afe94f85f29c958634ead9f3ba2dddf8e3ce803
Mark Chuang <markchuangth@gmail.com>
2022-01-20 08:48:49 -0500

    create a DayListItem.scss file to style the DayListItem component. Also, write unit tests to ensure that the messaging is correct for the spots feature. Also, write a function called formatSpots in the DayListItem component that will format the props.spots properly

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,31 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  let dayListItemClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected === true,
+    "day-list__item--full": props.full === 0
+
+  });
+
+  let formatSpots = (num) => {
+    if (num >1){
+      return num + " spots remaining"
+    } else if (num === 1) {
+      return "1 spot remaining";
+    } else if (num === 0) {
+      return "no spots remaining";
+    }
+  }
+  
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li 
+      onClick={() => props.setDay(props.name)}
+      className={dayListItemClass}
+    >
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)} </h3>
     </li>
   );
 }
