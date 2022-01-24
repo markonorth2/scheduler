52611a0272bbd03cf35533eb770a038b5ba9eda2
Mark Chuang <markchuangth@gmail.com>
2022-01-24 16:49:16 -0500

    create the components (Confirm, Status, Error) and the stories for them

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,9 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js"
 import Empty from "components/Appointment/Empty.js"
 import Show from "components/Appointment/Show.js"
+import Confirm from "components/Appointment/Confirm.js"
+import Status from "components/Appointment/Status.js"
+import Error from "components/Appointment/Error.js"
 
 
 storiesOf("Button", module)
@@ -149,3 +152,7 @@ storiesOf("Appointment", module)
   .add("Header", () => <Header time="12pm"/> )
   .add("Empty", () => <Empty onAdd={action("onAdd")}   />    )
   .add("Show", () => <Show student="Lydia Miller-Jones" interviewer= {interviewers} onEdit = {action("onEdit")} onDelete={action("onDelete")}/>)
+  .add("Confirm", ()=> <Confirm message="Delete the appointment?" onCancel={action("onCancel")} onConfirm={action("onConfirm")} /> )
+  .add("Status", () => <Status message={"Deleting"} />)
+  .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
+  
