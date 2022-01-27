33d01b0960381428902b04c8f20638619b345b77
Mark Chuang <markchuangth@gmail.com>
2022-01-26 22:22:41 -0500

    when user clicks on the add apointment button, it transitions to the CREATE mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,18 +4,15 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
 
 // Names for each mode
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
-const SAVING = "SAVING";
-const DELETING = "DELETING";
-const CONFIRM = "CONFIRM";
 const EDIT = "EDIT";
-const ERROR_SAVE = "ERROR_SAVE";
-const ERROR_DELETE = "ERROR_DELETE";
+
 
 export default function Appointment(props) {
   
@@ -27,15 +24,15 @@ export default function Appointment(props) {
       {/* {props.time ? `Appointment at ${props.time}` : "No Appointments"} */}
       <Header time={props.time}/>
 
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-        
+          onEdit={() => transition(EDIT)}
         />
        )}
-
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back(EMPTY)}/>}
       
     </article>
   )
