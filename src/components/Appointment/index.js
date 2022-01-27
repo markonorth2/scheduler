447de818eaf1fe43e17f00f18517fde9626f1aed
Mark Chuang <markchuangth@gmail.com>
2022-01-26 21:41:13 -0500

    use the useVisualMode hook to determine the mode and then conditionally render the matching component. Started with EMPTY and SHOW

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,12 +6,37 @@ import Show from "./Show";
 import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
 
+// Names for each mode
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
+
 export default function Appointment(props) {
+  
+  // useVisualMode is a custom hook to track mode. Transition is to update the mode and back is to rewind to the previous mode
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+  
   return(
     <article className="appointment">
       {/* {props.time ? `Appointment at ${props.time}` : "No Appointments"} */}
       <Header time={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        
+        />
+       )}
+
+      
     </article>
   )
 }
