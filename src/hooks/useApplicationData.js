12e258bfe1886279fb549d8fcbfb357247d596f8
Mark Chuang <markchuangth@gmail.com>
2022-01-30 10:19:40 -0500

    implemented the spots remaining feature

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -13,6 +13,36 @@ export default function useApplicationData() {
 
   const setDay = day => setState({...state, day});
 
+  function countSpots(appointments) {
+    let index = 0;
+    let dayObject = {};
+    // for each existing interview, we will take 1 away from remain 
+    let remain = 5;
+
+    //keep track of the index and store a copy of day object that is a match as state.day
+    for (let i = 0; i < state.days.length; i++) {
+      if (state.days[i].name === state.day) {
+        index = i;
+        dayObject = state.days[i]; 
+      }
+    }
+
+    //loop through appointment of the day and check if appointment are booked (interview exist or not). We count with remain variable
+    for (let appointment of dayObject.appointments) {
+      if (appointments[appointment].interview) {
+        remain -= 1;
+      }
+    }
+
+    let spotsUpdatedDayObject = {...dayObject, spots : remain};
+    let spotsUpdatedDaysArr = [...state.days];
+    spotsUpdatedDaysArr[index] = spotsUpdatedDayObject;
+
+    return spotsUpdatedDaysArr;
+  }
+
+
+
   function bookInterview(id, interview) {
 
     const appointment = {
@@ -25,9 +55,11 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
+    let updatedDays = countSpots(appointments);
+
     return axios.put(`/api/appointments/${id}`, appointment)
       .then(() =>   
-         setState((prev) => ({...prev, appointments})
+        setState((prev) => ({...prev, appointments, days : updatedDays}),
       )
     )
   };
@@ -42,11 +74,12 @@ export default function useApplicationData() {
       ...state.appointments,
       [id] : cancelAppointment
     }
+    let updatedDays = countSpots(appointments);
 
     return axios.delete(`/api/appointments/${id}`)
     .then(()=> 
-      setState({...state, appointments})
-    )
+      setState((prev) => ({...prev, appointments, days : updatedDays})
+    ))
   }
 
   useEffect(() => {
